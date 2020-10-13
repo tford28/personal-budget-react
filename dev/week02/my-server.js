51730abd4aa4d493e25e65b2691abf23c376c2e8
tford28 <48448772+tford28@users.noreply.github.com>
2020-10-13 15:48:56 -0400

    Delete my-server.js

--- dev/week02/my-server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.write('Hello World from NodeJS!');
-    res.end();
-}) .listen(3000, function(){
-    console.log("Server running at port 3000");
-});
