262888768a3745f519496c4de22b248409dc796b
Taylor Ford <tford28@uncc.edu>
2020-10-13 19:14:03 -0400

    Added Project Files

---
+++ personal-budget-react/src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className = "bottom" role= "Footer">
+        <div className = "center">
+            <a href = "./sitemap.html">Sitemap</a><br></br>
+            <a href = "./sitemap.xml">XML Sitemap</a><br></br>
+            All rights reserved &copy; Fabio Nolasco
+        </div>
+        </footer>
+    );
+}
+
+export default Footer;
